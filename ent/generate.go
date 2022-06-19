025e6ed0f5bc022771197150087798d3dfdf5955
Adil Chehabi <adil@chehabi.ma>
2022-06-19 11:22:17 -0400

    🐳 chore:add Upsert flag for generated entities

--- ent/generate.go
+++ ent/generate.go
@@ -1,3 +1,5 @@
 package ent
 
-//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate ./schema
+// Check https://entgo.io/docs/feature-flags/ for more information.
+// Upsert feature flags added in this file.
+//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate --feature sql/upsert ./schema
