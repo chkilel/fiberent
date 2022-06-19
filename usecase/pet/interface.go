83dd87e322d7b1edc1934f36bf60dfabe445c48e
Adil Chehabi <adil@chehabi.ma>
2022-06-19 13:23:10 -0400

    ✨ feat: create Pet Repository and Service interfaces

---
+++ usecase/pet/interface.go
@@ -0,0 +1,37 @@
+package pet
+
+import (
+	"context"
+
+	"github.com/chkilel/fiberent/entity"
+)
+
+// Reader interface
+type Reader interface {
+	Get(ctx context.Context, id *entity.ID) (*entity.Pet, error)
+	Search(ctx context.Context, query string) ([]*entity.Pet, error)
+	List(ctx context.Context) ([]*entity.Pet, error)
+}
+
+// Writer user writer
+type Writer interface {
+	Create(ctx context.Context, e *entity.Pet) (*entity.Pet, error)
+	Update(ctx context.Context, e *entity.Pet) (*entity.Pet, error)
+	Delete(ctx context.Context, id *entity.ID) error
+}
+
+// Repository interface
+type Repository interface {
+	Reader
+	Writer
+}
+
+// UseCase interface
+type UseCase interface {
+	GetPet(ctx context.Context, id *entity.ID) (*entity.Pet, error)
+	SearchPets(ctx context.Context, query string) ([]*entity.Pet, error)
+	ListPets(ctx context.Context) ([]*entity.Pet, error)
+	CreatePet(ctx context.Context, name string, age int) (*entity.Pet, error)
+	UpdatePet(ctx context.Context, e *entity.Pet) (*entity.Pet, error)
+	DeletePet(ctx context.Context, id *entity.ID) error
+}
