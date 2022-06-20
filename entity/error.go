a886d93766f29666936e1b778fd2886a35bea439
Adil Chehabi <adil@chehabi.ma>
2022-06-20 11:07:21 -0400

    feat:update errors

--- entity/error.go
+++ entity/error.go
@@ -17,5 +17,8 @@ var ErrCannotBeCreated = errors.New("cannot be created")
 //ErrCannotBeDeleted cannot be deleted
 var ErrCannotBeDeleted = errors.New("cannot be Deleted")
 
+//ErrCannotBeUpdated cannot be deleted
+var ErrCannotBeUpdated = errors.New("cannot be updated")
+
 //ErrCannotBeDeleted cannot be deleted
 var ErrPasswordGenaration = errors.New("pssword cannot be generated")
